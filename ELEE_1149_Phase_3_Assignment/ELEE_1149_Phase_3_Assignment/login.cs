480e17501f395c6a1657bfe07a47e6610c55091b
Cstratton-brown <98555569+Cstratton-brown@users.noreply.github.com>
2022-03-01 21:58:09 +0000

    Add files via upload

---
+++ ELEE_1149_Phase_3_Assignment/ELEE_1149_Phase_3_Assignment/login.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ELEE_1149_Phase_3_Assignment
+{
+    public partial class login : Form
+    {
+        
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\Charles\Documents\Visual Studio Projects\ELEE_1149_Phase_3_Assignment\ELEE_1149_Phase_3_Assignment\loginDatabase.mdf;Integrated Security = True");
+            SqlDataAdapter sda = new SqlDataAdapter("select count(*) from login where LoginID='" + txtID.Text + "'and LoginPassword='" + txtPassword.Text + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1") 
+            {
+                this.Hide();
+                new receptionistMenu().Show();
+            }
+            else
+            {
+                MessageBox.Show("Please enter correct Username and Password", "alert", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
