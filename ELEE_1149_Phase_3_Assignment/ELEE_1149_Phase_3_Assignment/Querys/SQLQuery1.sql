2208e31b20ee162576971237e95d20eceb274743
Cstratton-brown <98555569+Cstratton-brown@users.noreply.github.com>
2022-03-05 21:40:08 +0000

    Delete ELEE_1149_Phase_3_Assignment directory
    Deleted old version as folders and contents where renamed

--- ELEE_1149_Phase_3_Assignment/ELEE_1149_Phase_3_Assignment/Querys/SQLQuery1.sql
+++
@@ -1,5 +0,0 @@
-﻿SELECT Login.LoginID,
-	   Login.LoginPassword,
-	   Login.Role
-FROM Login
-WHERE CONVERT(VARCHAR,Login.Role) = 'Doctor'
--- ELEE_1149_Phase_3_Assignment/ELEE_1149_Phase_3_Assignment/Querys/SQLQuery2.sql
+++
@@ -1,5 +0,0 @@
-﻿SELECT Login.Role,
-	   Login.LoginID,
-	   Login.LoginPassword
-FROM Login
-WHERE CONVERT(VARCHAR,Login.LoginID) = '@txtID' and CONVERT(VARCHAR,Login.LoginPassword) = '@txtPassword'
